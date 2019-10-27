dda30349539e0c851893849bf3ba57a906391c12
crown523 <kxlider@rocketmail.com>
2019-10-27 02:57:36 -0400

    voting

--- react-stuff/src/GroupTranscript.js
+++ react-stuff/src/GroupTranscript.js
@@ -113,6 +113,12 @@ class GroupTranscript extends Component {
         }
       })
     });
+    const filterOut = ["", "vote", "and", "is", "to", "or", "the", "of", "a", "in", "that", "it", "for", "on"];
+    filterOut.forEach((element) => {
+      if (this.state.wordCount.has(element)) {
+        this.state.wordCount.delete(element);
+      }
+    })
     console.log(this.state.wordCount);
     this.setState({transcript:""});
   }
