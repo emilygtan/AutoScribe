193f217a4ef17ef54509ec22ee6ff5b612f5d356
cyu2019 <cyu2019@pingry.org>
2019-10-26 22:56:00 -0400

    basic send endpoint added

--- backend_test/app.js
+++ backend_test/app.js
@@ -3,10 +3,34 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var SummarizerManager = require("node-summarizer").SummarizerManager;
 
+const dotenv = require('dotenv');
+dotenv.config();
+
 server.listen(3000, function(){
 	console.log("listening on 3000");
 }); //listen on port 3000
 
+var APIMarketplaceClient = require('@collaborizm/apimarket/out').APIMarketplaceClient // for project authentication                                                           
+const apiMarketplaceClient = new APIMarketplaceClient({
+  grant_type: 'client_credentials',
+  client_id: process.env.CLIENT_ID,
+  client_secret: process.env.CLIENT_SECRET,
+  scope: 'openid'
+});
+
+// getValidToken will automatically refresh your access token if it's about to expire  
+var sendMsg = async (message, to) => {
+  const resp = await apiMarketplaceClient.simpleSmsSend({
+    message: message,
+    fromAddress: process.env.FROM,
+    toAddress: to,
+    callbackUrl: 'http://example.com'
+  })
+}
+app.get('/send',(req,res) => {
+	sendMsg(req.query.message, req.query.to)
+	res.end('success');
+})
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
