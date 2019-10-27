90a48ff7194a912f2b4d7414879b2c1554255f61
cyu2019 <cyu2019@pingry.org>
2019-10-27 00:40:09 -0400

    oaiwejfoiawejf

--- react-stuff/src/App.js
+++ react-stuff/src/App.js
@@ -16,44 +16,49 @@ import GroupTranscript from "./GroupTranscript.js"
 
 function App() {
   window.io = io;
-  React.useEffect(() => {
-    var connection = new RTCMultiConnection();
-    connection.socketURL = 'https://rtcmulticonnection.herokuapp.com:443/';
-    connection.session = {
-      audio: true,
-      video: true
-    };
-    connection.sdpConstraints.mandatory = {
-      OfferToReceiveAudio: true,
-      OfferToReceiveVideo: true
-    };
-    connection.onstream = function(event) {
-      var video = event.mediaElement;
-      video.controls = false;
-      video.style.cssText = "-moz-transform: scale(-1, 1); \
+
+  const [roomCode,setRoomCode] = React.useState("");
+
+  var connection = new RTCMultiConnection();
+  connection.socketURL = 'https://rtcmulticonnection.herokuapp.com:443/';
+  connection.session = {
+    audio: true,
+    video: true
+  };
+  connection.sdpConstraints.mandatory = {
+    OfferToReceiveAudio: true,
+    OfferToReceiveVideo: true
+  };
+  connection.onstream = function(event) {
+    var video = event.mediaElement;
+    video.controls = false;
+    video.style.cssText = "-moz-transform: scale(-1, 1); \
 -webkit-transform: scale(-1, 1); -o-transform: scale(-1, 1); \
 transform: scale(-1, 1); filter: FlipH;";
-      document.getElementById("callers").appendChild( event.mediaElement );
-    };
-    var predefinedRoomId = 'YOUR_Name';
+    document.getElementById("callers").appendChild( event.mediaElement );
+  };
+  const joinRoom = () => {
+    if (!roomCode)
+      return;
+    connection.checkPresence(roomCode, function(roomExist, roomid) {
+      alert('Room exists=' + roomExist);
+      if (roomExist === true) {
+        connection.join(roomCode);
+      } else {
+        connection.open(roomCode);
+      }
+    });
+  }
 
-    document.getElementById('btn-open-room').onclick = function() {
-      this.disabled = true;
-      connection.open( predefinedRoomId );
-    };
-
-    document.getElementById('btn-join-room').onclick = function() {
-      this.disabled = true;
-      connection.join( predefinedRoomId );
-    };
-
-  },[]);
   return (
     <div className="App">
-      <button id="btn-open-room">Open Room</button>
-      <button id="btn-join-room">Join Room</button><hr />
+      <div style={{display: roomCode? "none":"block"}}>
+        <input onChange={(e)=>setRoomCode(e.target.value)} />
+        <button onClick={joinRoom}>Join Room</button>
+      </div>
+      <hr />
       <div id="callers"></div>
-      <GroupTranscript />
+      <GroupTranscript roomCode={roomCode} active =  />
     </div>
   );
 }
--- react-stuff/src/GroupTranscript.js
+++ react-stuff/src/GroupTranscript.js
@@ -6,57 +6,33 @@ class GroupTranscript extends Component {
     super();
     this.state = {
       color: 'white',
-
       socket: openSocket("http://localhost:3000"),
-
       value: "type something!",
-
       messages: "<h1> chat transcript </h1>",
-
       transcript: "",
-
       summary: ""
     };
-
   }
-
   sendMsg = (text) => {
     console.log(text);
     this.state.socket.emit('message', text);
   }
-
   handleChange = (event) => {
     this.setState({value: event.target.value});
   }
-
   handleSend = () => {
     this.sendMsg(this.state.value);
     this.setState({value: ""});
   }
-
-// sending sockets
-  send = () => {
-    console.log("reached1");
-    this.state.socket.emit('change color', this.state.color) // change 'red' to this.state.color
-  }
-
-  // adding the function
-  setColor = (color) => {
-    this.setState({ color });
-    console.log(this.state.color);
-  }
-
   writeSummary = (text) => {
-    this.state.summary = "chat summary: \n" + text;
+    this.setState({summary: "chat summary: \n" + text});
   }
-
   summarizeReq = () => {
     var toSummarize = this.state.transcript;
-
     fetch("http://localhost:3000/summarize?body=" + toSummarize, {
       method: 'GET',
       //mode: 'no-cors',
-    }) 
+    })
       .then(res => res.text())
       .then(
         (result) => {
@@ -73,48 +49,33 @@ class GroupTranscript extends Component {
       )
     this.state.transcript = "";
   }
-
   componentDidMount= () => {
-
     var recognition = new window.webkitSpeechRecognition();
     recognition.continuous = true;
     recognition.interimResults = false;
-    recognition.lang = "cmn-Hans-CN";
+    recognition.lang = "en-US";
     recognition.onresult = event => {
       var transcript = event.results[event.results.length - 1][0].transcript
       console.log(transcript);
       this.sendMsg(transcript);
     }
-    recognition.start();
-
-
     this.state.socket.on('news', (data) => {
       console.log(data);
-    });
-    this.state.socket.on('change color', (col) => {
-      console.log("reached");
-      document.body.style.backgroundColor = col;
     });
     this.state.socket.on('message', (data) => {
       console.log("message received: " + data);
       this.setState({transcript:this.state.transcript+"\n"+data});
       console.log(this.state.transcript);
     });
-
-    
-
-
   }
 
   render() {
-
+    if (!this.props.roomCode)
+      return null;
     return (
       <div style={{ textAlign: "center" }}>
         <input type="text" value={this.state.value} onChange={this.handleChange}/>
         <button onClick={this.handleSend}> send </button>
-        <button onClick={() => this.send() }>Change Color</button>
-        <button id="blue" onClick={() => this.setColor('blue')}>Blue</button>
-        <button id="red" onClick={() => this.setColor('red')}>Red</button>
         <div>
           <button id="summarize" onClick={this.summarizeReq}>Summarize Meeting</button>
           <p>{this.state.summary}</p>
